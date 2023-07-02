f79c2b95e35542f852e2fff9f5c6d92f6665194c
murakami <murashoya@gmail.com>
2023-07-02 12:55:52 +0900

    [fix] flatten_multilineとconcatを別関数に分割
    同時にやると、while文の関係で飛ばされる時があるため

--- src/create_datset_from_line.py
+++ src/create_datset_from_line.py
@@ -9,12 +9,12 @@ def txt_to_list(txt_file: str) -> list:
 
 def extract_only_chat(txt_list: list) -> list:
     """
-    日付、改行、写真、スタンプの行を削除
-    電話、不在着信、絵文字の削除、リンク
+    日付、改行、写真、スタンプの行を削除、電話、不在着信、リンク
+    絵文字、""の削除
     """
     extracted_list = []
     deleted_re = re.compile(r'(^\d{4}\/\d{2}\/\d{2})|(^\n)|(^\")|\[写真\]|\[スタンプ\]|☎|https?://[\w!\?/\+\-_~=;\.,\*&@#\$%\(\)\'\[\]]+')
-    deleted_sub_re = re.compile(r'[ .0-9a-zA-Z]*\(([\\!"#\\$%&\'\()*+,-./:;<=>?@\[\]^_`{}~ ﾟ「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％∀▽´｀┐εཀ∠]+[0-9a-zA-Z]*)+\)[ .0-9a-zA-Z]*')
+    deleted_sub_re = re.compile(r'([ .0-9a-zA-Z]*\(([\\!"#\\$%&\'\()*+,-./:;<=>?@\[\]^_`{}~ ﾟ「」〔〕“”〈〉『』【】＆＊・（）＄＃＠。、？！｀＋￥％∀▽´｀┐εཀ∠]+[0-9a-zA-Z]*)+\)[ .0-9a-zA-Z]*)|"')
 
     for row in txt_list:
         if not (deleted_re.search(row)):
@@ -22,13 +22,11 @@ def extract_only_chat(txt_list: list) -> list:
 
     return extracted_list
 
-def flatten_multiline_and_concat(txt_list: list) -> list:
+def flatten_multiline(txt_list: list) -> list:
     """
     複数行のチャットを一つにまとめる
-    連続した人のチャットは\nで区切って結合する
     """
     i = 0
-    pre_name = ""
     chat_head_re = "\d{2}:\d{2}\t"   # チャットは00:00のフォーマットで始まる
 
     while i < len(txt_list):
@@ -36,12 +34,27 @@ def flatten_multiline_and_concat(txt_list: list) -> list:
         if not (re.match(chat_head_re, txt_list[i])):
             txt_list[i-1] = txt_list[i-1] + txt_list[i]
             txt_list.remove(txt_list[i])
-            continue
+        else: i += 1
 
+    return txt_list
+
+def concat_and_detail(txt_list):
+    # 連続した人のチャットは\nで区切って結合する
+    i = 0
+    pre_name = ""
+    yahoo_transit_re = re.compile(r'-------')
+
+    while i < len(txt_list):
         # 交互に1つずつのチャットなるように(A→B→A→B→…)
         # チャットのフォーマットは [00:00	名前	内容]
         try:
             _, name, message = txt_list[i].split("\t", 2)
+
+            # ついでにyahoo乗換案内は削除
+            if (re.search(yahoo_transit_re, message)):
+                txt_list.remove(txt_list[i])
+                continue
+
             if(name == pre_name):
                 txt_list[i-1] = txt_list[i-1] + message
                 txt_list.remove(txt_list[i])
@@ -49,17 +62,19 @@ def flatten_multiline_and_concat(txt_list: list) -> list:
                 pre_name = name
                 i += 1
         except:
-            print(txt_list[i])
+            print('error', txt_list[i])
             
-
     return txt_list
 
-def convert_to_json(lst:list, boy_name:str, girl_name:str) -> None:
+
+# def finalize_processing(txt_list: list) -> list:
+#     return txt_list
+
+def convert_to_json(txt_list:list, boy_name:str, girl_name:str) -> None:
     dict_list = []
 
     dict = {"time": "", "speaker": "", "text": ""}
-    for i in range(0, len(lst)):
-        line = lst[i]
+    for line in txt_list:
         time, name, message = line.split("\t", 2)
 
         dict['time'] = time
@@ -72,4 +87,13 @@ def convert_to_json(lst:list, boy_name:str, girl_name:str) -> None:
 
     with open('chat_dataset.json', 'w') as f:
         json.dump(dict_list, f, indent=2)
-    
+
+
+# txt_list = txt_to_list('./small_chat_dataset.txt')
+# extracted_list = extract_only_chat(txt_list)
+# extracted_list = flatten_multiline(extracted_list)
+# extracted_list = concat_and_detail(extracted_list)
+
+# print(extracted_list[-10:])
+# extracted_list = finalize_processing(extracted_list)
+# convert_to_json(extracted_list, boy_name='村上翔哉', girl_name='山本　理央')
